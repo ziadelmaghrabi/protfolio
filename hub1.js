43ffd1496fd1d1df173558c3939dfc90dd323b9e
ziad ayman elmaghrabi <161942240+ziadelmaghrabi@users.noreply.github.com>
2025-11-24 23:17:28 +0200

    Update hub1.js

--- hub1.js
+++ hub1.js
@@ -33,3 +33,20 @@ aboutSection.addEventListener("mouseenter", () => {
     type();
   }
 });
+
+ const img = document.querySelector('.profile-img');
+    img.addEventListener('mousemove', function(e) {
+        const rect = img.getBoundingClientRect();
+        const x = e.clientX - rect.left - rect.width / 2;
+        const y = e.clientY - rect.top - rect.height / 2;
+        img.style.transform = `rotateY(${x / 20}deg) rotateX(${-y / 20}deg) scale(1.05)`;
+        img.style.transition = 'transform 0.1s';
+        img.style.boxShadow = `0 8px 40px 0 #4fc3f7, 0 6px 24px rgba(0,0,0,0.8)`;
+    });
+    img.addEventListener('mouseleave', function() {
+        img.style.transform = 'rotateY(0deg) rotateX(0deg) scale(1)';
+        img.style.transition = 'transform 0.4s';
+        img.style.boxShadow = '0 6px 24px rgba(0,0,0,0.8)';
+    });
+
+
